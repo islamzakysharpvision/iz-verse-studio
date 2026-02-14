dcde01eda13dec9f72359260536fe233cd23e1ed
islamzakysharpvision <54447465+islamzakysharpvision@users.noreply.github.com>
2026-02-14 19:24:39 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -182,3 +182,13 @@ window.addEventListener("click", (e) => {
   }
 });
 
+/* ================= SERVICE WORKER ================= */
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch((err) => console.log("SW registration failed:", err));
+  });
+}
+
